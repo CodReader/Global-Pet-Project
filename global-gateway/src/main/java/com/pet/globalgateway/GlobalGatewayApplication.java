9d72814955a722d0f4377638b02a87a1fb6b892d
bogdan <b.synenko@vector-inc.dev>
2025-03-04 18:50:45 +0200

    api gateway module creation

---
+++ global-gateway/src/main/java/com/pet/globalgateway/GlobalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pet.globalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalGatewayApplication.class, args);
+	}
+
+}
---
+++ global-gateway/src/test/java/com/pet/globalgateway/GlobalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.globalgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
