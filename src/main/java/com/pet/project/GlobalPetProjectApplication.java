0d3aa51c906d8e68e0735e5e3dda55ee028549b5
bogdan <b.synenko@vector-inc.dev>
2025-03-04 18:34:45 +0200

    initial global project

---
+++ src/main/java/com/pet/project/GlobalPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pet.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalPetProjectApplication.class, args);
+    }
+
+}
